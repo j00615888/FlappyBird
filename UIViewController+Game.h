57d1984b129e50d12fc94cf69e9b6a6b91ef740a
larry <j00615888@students.jsums.edu>
2015-06-24 11:53:25 -0500

    New Images. Game class. Main Menu 1.0.

---
+++ UIViewController+Game.h
@@ -0,0 +1,13 @@
+//
+//  UIViewController+Game.h
+//  FlappyBird
+//
+//  Created by Key on 6/24/15.
+//  Copyright (c) 2015 Key. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIViewController (Game)
+
+@end
---
+++ UIViewController+Game.m
@@ -0,0 +1,13 @@
+//
+//  UIViewController+Game.m
+//  FlappyBird
+//
+//  Created by Key on 6/24/15.
+//  Copyright (c) 2015 Key. All rights reserved.
+//
+
+#import "UIViewController+Game.h"
+
+@implementation UIViewController (Game)
+
+@end
